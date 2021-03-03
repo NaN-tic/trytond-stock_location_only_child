cb15cf4f47f4e62c475650819da3fb4010cd0cb3
MarinaNico <marina.nicolau@nan-tic.com>
2021-03-03 15:15:53 +0100

    Review code fixes

--- stock.py
+++ stock.py
@@ -1,7 +1,5 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import functools
-
 from trytond.i18n import gettext
 from trytond.model import fields
 from trytond.pool import PoolMeta
@@ -21,12 +19,13 @@ class Location(metaclass=PoolMeta):
     def validate(cls, locations):
         super(Location, cls).validate(locations)
         for location in locations:
-            if location.parent:
-                if (location.parent.unique_children and
-                    len(location.parent.childs) > 1):
-                    raise UserError(
-                        gettext('stock_location_only_child.msg_only_child'))
+            if (location.parent and location.parent.unique_children and
+                len(location.parent.childs) > 1):
+                raise UserError(
+                    gettext('stock_location_only_child.msg_only_child',
+                        location=location.rec_name))
 
             if location.unique_children and len(location.childs) > 1:
                 raise UserError(
-                    gettext('stock_location_only_child.msg_only_child'))
+                    gettext('stock_location_only_child.msg_only_child',
+                        location=location.rec_name))
